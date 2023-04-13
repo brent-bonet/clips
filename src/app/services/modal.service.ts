9c81037dc8ef6b4db73872e787ee9c3fa5914100
Brent Bonet <brent@brentbonet.com>
2023-04-13 11:43:08 -0600

    6-97

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -9,7 +9,7 @@ interface IModal {
   providedIn: 'root',
 })
 export class ModalService {
- private modals: IModal[] = [];
+  private modals: IModal[] = [];
 
   constructor() {}
 
@@ -18,21 +18,17 @@ export class ModalService {
   }
 
   toggleModal(id: string): void {
-    console.log('toggle', id);
     const modal = this.modals.find((element) => element.id === id);
-    console.log('modal', modal);
     if (modal) {
       modal.visible = !modal.visible;
-      console.log('modal after toggle', modal);
     }
   }
 
   register(id: string): void {
-    console.log('register', id);
     this.modals.push({ id, visible: false });
   }
 
-  unregister(id: string):  void {
-    this.modals = this.modals.filter( element => element.id !== id)
+  unregister(id: string): void {
+    this.modals = this.modals.filter((element) => element.id !== id);
   }
 }
--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
 })
 export class TabComponent {
+  @Input() public tabTitle = '';
+  @Input() active = false;
 
+  ngOnInit(): void {}
 }
