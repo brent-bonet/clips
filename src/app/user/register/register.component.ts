13ae65a2891628e1af3a39f3e9f786a7023c3fbc
Brent Bonet <brent@brentbonet.com>
2023-04-17 11:33:54 -0600

    7-111

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -8,8 +8,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('');
-  age = new FormControl('');
+  email = new FormControl('', [Validators.required, Validators.email]);
+  age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]);
   password = new FormControl('');
   confirm_password = new FormControl('');
   phoneNumber = new FormControl('');
