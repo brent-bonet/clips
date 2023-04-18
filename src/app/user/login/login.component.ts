5bbc7088a55a8a06b4497979fd7faefc680626e6
Brent Bonet <brent@brentbonet.com>
2023-04-18 12:44:53 -0600

    8-125

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   credentials = { email: '', password: '' };
+
+  login() {
+    console.log('login', this.credentials);
+  }
 }
