cb1d88b844aa4633301d7d76512bf8f6d7de054b
Brent Bonet <brent@brentbonet.com>
2023-04-18 11:24:48 -0600

    7-117

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,12 +6,13 @@ import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskDirective, NgxMaskPipe, NgxMaskService } from 'ngx-mask';
+import { AlertComponent } from './alert/alert.component';
 // import { ModalService } from '../services/modal.service';
 
 @NgModule({
-  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent, AlertComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxMaskDirective],
-  exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent, InputComponent, AlertComponent],
   providers: [NgxMaskService],
 })
 export class SharedModule {}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,12 +7,16 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
   age = new FormControl('', [Validators.required, Validators.min(18), Validators.max(120)]);
   password = new FormControl('', [
     Validators.required,
-    Validators.pattern('/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm'),
+    Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'),
   ]);
   confirm_password = new FormControl('', [Validators.required]);
   phoneNumber = new FormControl('', [Validators.required, Validators.minLength(13), Validators.maxLength(13)]);
@@ -27,6 +31,8 @@ export class RegisterComponent {
   });
 
   register() {
-    console.log('submitted');
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
   }
 }
