757481cd2c72b8acd75fd37b4323eab4ca3f6775
Brent Bonet <brent@brentbonet.com>
2023-04-28 10:03:50 -0600

    12-190

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import IUser from '../models/user.model';
 import { Observable } from 'rxjs';
 import { map, delay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore, private router: Router) {
     this.usersCollection = db.collection<IUser>('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
@@ -41,5 +42,13 @@ export class AuthService {
     await userCredential.user.updateProfile({
       displayName: userData.name,
     });
+  }
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+    await this.router.navigateByUrl('/');
   }
 }
