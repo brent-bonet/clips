9b67858421eed473ed42705b1a421eecd32f25f3
Brent Bonet <brent@brentbonet.com>
2023-05-05 09:56:43 -0600

    13-213

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-upload',
@@ -22,6 +23,7 @@ export class UploadComponent {
   alertMessage = 'Please wait! Your clip is being uploaded';
   inSubmission = false;
   percentage = 0;
+  showPercentage = false;
 
   constructor(private storage: AngularFireStorage) {}
 
@@ -40,6 +42,7 @@ export class UploadComponent {
     this.alertColor = 'blue';
     this.alertMessage = 'Please wait! Your clip is being uploaded';
     this.inSubmission = true;
+    this.showPercentage = true;
 
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
@@ -47,5 +50,23 @@ export class UploadComponent {
     task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
+
+    task
+      .snapshotChanges()
+      .pipe(last())
+      .subscribe({
+        next: (snapshot) => {
+          this.alertColor = 'green';
+          this.alertMessage = 'Success! You clip is now ready to share with the world.';
+          this.showPercentage = false;
+        },
+        error: (error) => {
+          this.alertColor = 'red';
+          this.alertMessage = 'Upload failed! Please try again later';
+          this.inSubmission = true;
+          this.showPercentage = false;
+          console.log(error);
+        },
+      });
   }
 }
