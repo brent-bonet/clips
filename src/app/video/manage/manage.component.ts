585a9e7d6856dedc67e3292a1b649ca8a9c9645f
Brent Bonet <brent@brentbonet.com>
2023-05-05 13:44:31 -0600

    13-223

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ClipService } from '../../services/clip.service';
+import IClip from '../../models/clip.model';
 
 @Component({
   selector: 'app-manage',
@@ -8,12 +10,26 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 })
 export class ManageComponent {
   videoOrder = '1';
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  clips: IClip[] = [];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipsService: ClipService
+  ) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(
       (params: Params) => (this.videoOrder = params.sort === '2' ? params.sort : '1')
     );
+    this.clipsService.getUserClips().subscribe((docs) => {
+      this.clips = [];
+      docs.forEach((doc) => {
+        this.clips.push({
+          docId: doc.id,
+          ...doc.data(),
+        });
+      });
+    });
   }
   sort(event: Event) {
     const { value } = event.target as HTMLSelectElement;
