58efeaa0c5763f8db6643d830deee39a60394101
Brent Bonet <brent@brentbonet.com>
2023-05-05 10:12:03 -0600

    13-214

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs';
+import { last, switchMap } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
@@ -24,8 +26,11 @@ export class UploadComponent {
   inSubmission = false;
   percentage = 0;
   showPercentage = false;
+  user: firebase.User | null = null;
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(private storage: AngularFireStorage, private auth: AngularFireAuth) {
+    auth.user.subscribe((user) => (this.user = user));
+  }
 
   storeFile($event: DragEvent) {
     this.isDragover = false;
@@ -47,18 +52,30 @@ export class UploadComponent {
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
     const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
     task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
     });
 
     task
       .snapshotChanges()
-      .pipe(last())
+      .pipe(
+        last(),
+        switchMap(() => clipRef.getDownloadURL())
+      )
       .subscribe({
-        next: (snapshot) => {
-          this.alertColor = 'green';
-          this.alertMessage = 'Success! You clip is now ready to share with the world.';
-          this.showPercentage = false;
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid,
+            displayName: this.user?.displayName,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+          console.log(clip);
+          // this.alertColor = 'green';
+          // this.alertMessage = 'Success! You clip is now ready to share with the world.';
+          // this.showPercentage = false;
         },
         error: (error) => {
           this.alertColor = 'red';
