6539d25c9cd94e856a33d633c1850f5aa142a0c5
Brent Bonet <brent@brentbonet.com>
2023-05-10 11:38:48 -0600

    15-265

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { writeFile } from 'fs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,10 @@ export class FfmpegService {
     await this.ffmpeg.load();
 
     this.isReady = true;
+  }
+
+  async getScreenshots(file: File) {
+    const data = await fetchFile(file);
+    this.ffmpeg.FS('writeFile', file.name, data);
   }
 }
--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -48,7 +48,7 @@ export class UploadComponent implements OnDestroy {
     this.task?.cancel();
   }
 
-  storeFile($event: Event) {
+  async storeFile($event: Event) {
     this.isDragover = false;
     this.file = ($event as DragEvent).dataTransfer
       ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
@@ -56,6 +56,8 @@ export class UploadComponent implements OnDestroy {
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
     }
+
+    await this.ffmpegService.getScreenshots(this.file);
     this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
   }
